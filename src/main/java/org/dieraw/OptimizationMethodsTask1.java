dcf9172077f4d00b4b16ace9ad8192e071c4a2fb
dieraw <dieasrdie@gmail.com>
2025-05-17 09:28:52 +0300

    updated fibonacciSearch

--- src/main/java/org/dieraw/OptimizationMethodsTask1.java
+++ src/main/java/org/dieraw/OptimizationMethodsTask1.java
@@ -7,20 +7,18 @@ import java.math.RoundingMode;
 public class OptimizationMethodsTask1 {
 
     private static final double E = Math.E;
-    private static final double EPSILON = 0.001;
-    private static final double GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
+    private static final double EPSILON = 0.01;
     private static int functionEvaluations;
 
     public static void main(String[] args) {
         System.out.println("Задача 17.45:");
         System.out.println("f(x) = x^2 + 2 * (x * lg(x/e) - 2)");
         System.out.println("Интервал: [1.5, 2], Эпсилон: " + EPSILON);
+//        double a = 1.5;
+//        double b = 2.0;
 
-        double a = 1.5;
-        double b = 2.0;
-
-//        double a = 3;
-//        double b = 3.5;
+        double a = 3.0;
+        double b = 3.5;
 
         // 1. Метод пассивного поиска
         functionEvaluations = 0;
@@ -45,8 +43,9 @@ public class OptimizationMethodsTask1 {
 
     private static double f(double x) {
         functionEvaluations++;
-        return (Math.pow(x, 2) + 2 * (x * (Math.log10((x / E))) - 2));
-//        return (5 * Math.pow(x, 2) - 8 * Math.pow(x, 1.25) - 20 * x);
+//        return (Math.pow(x, 2) + 2 * (x * (Math.log10(x / E)) - 2));
+        return (5 * Math.pow(x, 2) - 8 * Math.pow(x, 1.25) - 20 * x); // другой вариант
+
     }
 
     // 1. Метод пассивного поиска
@@ -69,13 +68,13 @@ public class OptimizationMethodsTask1 {
     // 2. Метод дихотомии
     private static double dichotomy(double a, double b, double epsilon) {
         double delta = epsilon / 2.0;
-        while (b - a >= 2 * epsilon) {
-            double c = (a + b) / 2.0 -  delta / 2.0;
-            double d = (a + b) / 2.0 +  delta / 2.0;
+        while (b - a > 2 * epsilon) { // Критерий остановки: длина интервала > 2 * epsilon
+            double c = (a + b) / 2.0 - delta;
+            double d = (a + b) / 2.0 + delta;
             if (f(c) <= f(d)) {
-                b = d;
+                b = d; // Сужаем интервал справа, если f(c) <= f(d)
             } else {
-                a = c;
+                a = c; // Сужаем интервал слева, если f(c) > f(d)
             }
         }
         return (a + b) / 2.0;
@@ -83,96 +82,95 @@ public class OptimizationMethodsTask1 {
 
     // 3. Метод золотого сечения
     private static double goldenSectionSearch(double a, double b, double epsilon) {
-        double x1 = b - (b - a) / GOLDEN_RATIO;
-        double x2 = a + (b - a) / GOLDEN_RATIO;
+        // Константа золотого сечения
+        final double GOLDEN_RATIO = (Math.sqrt(5) - 1) / 2; // Примерно 0.618
+
+        double x1 = b - GOLDEN_RATIO * (b - a);
+        double x2 = a + GOLDEN_RATIO * (b - a);
         double f1 = f(x1);
         double f2 = f(x2);
 
-        while (b - a > epsilon) {
-            if (f1 <= f2) {
+        int iterations = 0;
+        while (b - a > epsilon) { // Добавляем ограничение на итерации
+            iterations++;
+            if (f1 < f2) { // Если f(x1) < f(x2), то минимум находится в интервале [a, x2]
                 b = x2;
                 x2 = x1;
                 f2 = f1;
-                x1 = b - (b - a) / GOLDEN_RATIO;
+                x1 = b - GOLDEN_RATIO * (b - a);
                 f1 = f(x1);
-            } else {
+            } else {  //Если f(x1) >= f(x2), то минимум находится в интервале [x1, b]
                 a = x1;
                 x1 = x2;
                 f1 = f2;
-                x2 = a + (b - a) / GOLDEN_RATIO;
+                x2 = a + GOLDEN_RATIO * (b - a);
                 f2 = f(x2);
             }
         }
         return (a + b) / 2.0;
     }
 
-    private static double fibonacciSearch(double a, double b, double epsilon) {
-        int n = calculateFibonacciIterations(a, b, epsilon);
-        if (n <= 2) return (a + b) / 2.0;
-
-        double[] fib = fibonacciSequence(n + 3); // Увеличиваем длину массива до n+3
-
-        double c = a + (b - a) * fib[n] / fib[n + 2]; // Индексы n и n+2 (Строка 111 - Исправлено)
-        double d = a + (b - a) * fib[n + 1] / fib[n + 2]; // Индексы n+1 и n+2 (Строка 112 - Исправлено)
-
-
-        double fc = f(c);
-        double fd = f(d);
-
-        for (int k = 1; k <= n; k++) { // Цикл до n (без изменений)
-            if (fc <= fd) {
-                b = d;
-                d = c;
-                fd = fc;
-                c = a + (b - a) * fib[n - k] / fib[n + 2 - k]; // Индексы n-k и n+2-k (Строка 119 - Исправлено)
-                fc = f(c);
-
-
-            } else {
-                a = c;
-                c = d;
-                fc = fd;
-                d = a + (b - a) * fib[n + 1 - k] / fib[n + 2 - k]; // Индексы n+1-k и n+2-k (Строка 126 - Исправлено)
-                fd = f(d);
-            }
-        }
-        return (a + b) / 2.0;
-    }
-
-
-    private static int calculateFibonacciIterations(double a, double b, double epsilon) {
-        int n = 1;
-        double fn = 1;
-        while ((b - a) / fn > epsilon) {
-            n++;
-            fn = getFibonacci(n);
-        }
-        return n;
-    }
-
-    private static double getFibonacci(int n) {
-        if (n <= 1) return 1;
-        double[] fib = fibonacciSequence(n+2);
-        return fib[n-1];
-    }
-
-    private static double[] fibonacciSequence(int n) {
-        if (n <= 0) return new double[0];
-        if (n == 1) return new double[]{1};
-        if (n == 2) return new double[]{1, 1};
-        double[] fib = new double[n];
+    private static double fibonacciSearch(double a, double b, double epsilon) {double[] fib = new double[100];
         fib[0] = 1;
         fib[1] = 1;
-        for (int i = 2; i < n; i++) {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-        return fib;
-    }
 
+        int n = 1;
+        for (int i = 2; i < 100; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+            if (fib[i] >= (b - a) / epsilon) {
+                n = i;
+                break;
+            }
+        }
+
+        if (n < 3) n = 3;
+
+        double a_n = a;
+        double b_n = b;
+
+        double x1 = a_n + (fib[n-2] / fib[n]) * (b_n - a_n);
+        double x2 = a_n + (fib[n-1] / fib[n]) * (b_n - a_n);
+
+        double f1 = f(x1);
+        double f2 = f(x2);
+
+        for (int k = 1; k <= n-3; k++) {
+            if (f1 > f2) {
+                a_n = x1;
+                x1 = x2;
+                f1 = f2;
+                if (n-k-1 >= 0 && n-k >= 0) {
+                    x2 = a_n + (fib[n-k-1] / fib[n-k]) * (b_n - a_n);
+                    f2 = f(x2);
+                }
+            } else {
+                b_n = x2;
+                x2 = x1;
+                f2 = f1;
+                if (n-k-2 >= 0 && n-k >= 0) {
+                    x1 = a_n + (fib[n-k-2] / fib[n-k]) * (b_n - a_n);
+                    f1 = f(x1);
+                }
+            }
+        }
+    // Обработка финальных сравнений
+    // Вместо того чтобы пытаться вычислить новую точку, делаем необольшой шаг
+        double delta = epsilon / 10.0;
+        x2 = x1 + delta;
+        f2 = f(x2);
+
+        if (f1 > f2) {
+            a_n = x1;
+        } else {
+            b_n = x2;
+        }
+
+        return (a_n + b_n) / 2.0;
+    }
 
     private static String formatDouble(double d) {
         BigDecimal bd = BigDecimal.valueOf(d);
-        bd = bd.round(new MathContext(7, RoundingMode.HALF_UP));
+        bd = bd.round(new MathContext(9, RoundingMode.HALF_UP));
         return bd.toString();
     }
 }
